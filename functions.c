53198ee4e72d35b581c5d9e6851463fb67c7bc9c
justissy17 <isrealuka@gmail.com>
2023-07-28 18:27:50 -0700

    printf

---
+++ functions.c
@@ -0,0 +1,188 @@
+#include "main.h"
+
+/************************* PRINT CHAR *************************/
+
+/**
+ * print_char - Prints a char
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	char c = va_arg(types, int);
+
+	return (handle_write_char(c, buffer, flags, width, precision, size));
+}
+/** PRINT A STRING **/
+/**
+ * print_string - Prints a string
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	int length = 0, x;
+	char *str = va_arg(types, char *);
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	if (str == NULL)
+	{
+		str = "(null)";
+		if (precision >= 6)
+			str = "      ";
+	}
+
+	while (str[length] != '\0')
+		length++;
+
+	if (precision >= 0 && precision < length)
+		length = precision;
+
+	if (width > length)
+	{
+		if (flags & F_MINUS)
+		{
+			write(1, &str[0], length);
+			for (x = width - length; x > 0; x--)
+				write(1, " ", 1);
+			return (width);
+		}
+		else
+		{
+			for (x = width - length; x > 0; x--)
+				write(1, " ", 1);
+			write(1, &str[0], length);
+			return (width);
+		}
+	}
+
+	return (write(1, str, length));
+}
+/*** PRINT PERCENT SIGN ***/
+/**
+ * print_percent - Prints a percent sign
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int print_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	UNUSED(types);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	return (write(1, "%%", 1));
+}
+
+/**** PRINT INT ****/
+/**
+ * print_int - Print int
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	int x = BUFF_SIZE - 2;
+	int is_negative = 0;
+	long int w = va_arg(types, long int);
+	unsigned long int num;
+
+	w = convert_size_number(w, size);
+
+	if (w == 0)
+		buffer[x--] = '0';
+
+	buffer[BUFF_SIZE - 1] = '\0';
+	num = (unsigned long int)w;
+
+	if (w < 0)
+	{
+		num = (unsigned long int)((-1) * w);
+		is_negative = 1;
+	}
+
+	while (num > 0)
+	{
+		buffer[x--] = (num % 10) + '0';
+		num /= 10;
+	}
+	
+	x++;
+
+	return (write_number(is_negative, x, buffer, flags, width, precision, size));
+}
+
+/**** PRINT BINARY ****/
+/**
+ * print_binary - Prints an unsigned number
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Numbers of char printed.
+ */
+int print_binary(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	unsigned int w, y, x, sum;
+	unsigned int arr[32];
+	int count;
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	w = va_arg(types, unsigned int);
+	y = 2147483648; /* (2 ^ 31) */
+	arr[0] = w / y;
+	for (x = 1; x < 32; x++)
+	{
+		y /= 2;
+		arr[i] = (w / y) % 2;
+	}
+	for (x = 0, sum = 0, count = 0; x < 32; x++)
+	{
+		sum += arr[x];
+		if (sum || x == 31)
+		{
+			char p = '0' + arr[x];
+
+			write(1, &p, 1);
+			count++;
+		}
+	}
+	return (count);
+}
